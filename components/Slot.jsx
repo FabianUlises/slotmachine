21f5116b019fb6397ffae9c14abef3163895d4cd
Fabian ulises Carrillo <ulisesfab.car@outlook.com>
2024-06-11 19:08:08 -0700

    Moved app logic to upper component and passed down data as props.

--- components/Slot.jsx
+++ components/Slot.jsx
@@ -1,35 +1,19 @@
 class Slot extends React.Component {
     render() {
-        // Getting random number to get array index
-        const randomNum1 = Math.floor(Math.random() * this.props.slotImg.length);
-        const randomNum2 = Math.floor(Math.random() * this.props.slotImg.length);
-        const randomNum3 = Math.floor(Math.random() * this.props.slotImg.length);
         return (
             // Start of slot container
             <div className="slot">
                 {/* Start of slot image */}
                 <div className="slot__img">
                     {/* Setting image src to random array index. If image is index 0 I'm giving it a class to lower width size */}
-                    <img src={this.props.slotImg[randomNum1]} className={randomNum1 === 0 ? 'large-cherry' : null} />
-                    <img src={this.props.slotImg[randomNum2]} className={randomNum2 === 0 ? 'large-cherry' : null} />
-                    <img src={this.props.slotImg[randomNum3]} className={randomNum3 === 0 ? 'large-cherry' : null}/>
+                    <img src={this.props.img1} />
+                    <img src={this.props.img2} />
+                    <img src={this.props.img3} />
                 </div>
                 {/* End of slot image */}
                 {/* Start of slot text */}
                 <div className="slot__text">
-                    {
-                        randomNum1 === randomNum2 && randomNum1 === randomNum3 ? 
-                            (
-                                // <div>
-                                    <h3>You Win!</h3>
-                                // </div>
-                            )
-                            : (
-                                // <div>
-                                    <h3>You Lose!</h3>
-                                // </div>
-                            )
-                    }
+                    {this.props.outcome}
                 </div>
                 {/* End of slot text */}
             </div>
--- components/SlotMachine.jsx
+++ components/SlotMachine.jsx
@@ -1,9 +1,28 @@
 class SlotMachine extends React.Component {
     render() {
+        const slotImg = ['../img/cherry-1.png', '../img/cherry-2.png', '../img/cherry-3.png'];
+        // Getting random number to get array index
+        const randomNum1 = Math.floor(Math.random() * slotImg.length);
+        const randomNum2 = Math.floor(Math.random() * slotImg.length);
+        const randomNum3 = Math.floor(Math.random() * slotImg.length);
+        const outcome = randomNum1 === randomNum2 && randomNum1 === randomNum3 ? 
+                (
+                    // <div>
+                        <h3>You Win!</h3>
+                    // </div>
+                )
+                : (
+                    // <div>
+                        <h3>You Lose!</h3>
+                    // </div>
+                )
         return (
             <div>
                 <Slot
-                    slotImg={['../img/cherry-1.png', '../img/cherry-2.png', '../img/cherry-3.png']}
+                    img1={slotImg[randomNum1]}
+                    img2={slotImg[randomNum2]}
+                    img3={slotImg[randomNum3]}
+                    outcome={outcome}
                  />
             </div>
         )
