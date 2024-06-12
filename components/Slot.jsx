22925f16c817c5ac3244817b6290da5c923ec4cb
fabian ulises <ulises.fab@outlook.com>
2024-06-11 17:22:51 -0700

    Updated main css file &  slot jsx component file. Generating two more random numbers to use for random array index.

--- components/Slot.jsx
+++ components/Slot.jsx
@@ -1,12 +1,14 @@
 class Slot extends React.Component {
     render() {
         const randomNum1 = Math.floor(Math.random() * this.props.slotImg.length);
+        const randomNum2 = Math.floor(Math.random() * this.props.slotImg.length);
+        const randomNum3 = Math.floor(Math.random() * this.props.slotImg.length);
         return (
             <div className="slot">
                 <div className="slot__img">
                     <img src={this.props.slotImg[randomNum1]} />
-                    <img src={this.props.slotImg[randomNum1]} />
-                    <img src={this.props.slotImg[randomNum1]} />
+                    <img src={this.props.slotImg[randomNum2]} />
+                    <img src={this.props.slotImg[randomNum3]} />
                 </div>
                 <div className="slot__text">
                     You lose!
